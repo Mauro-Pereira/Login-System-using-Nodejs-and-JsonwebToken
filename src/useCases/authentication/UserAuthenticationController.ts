33906115faf45dbbe931bfd8d1cf85f753d04429
Mauro-Pereira <mauropereira1096@gmail.com>
2021-02-04 20:57:37 -0300

    add handle method

---
+++ src/useCases/authentication/UserAuthenticationController.ts
@@ -0,0 +1,23 @@
+import {Request,Response} from 'express';
+import {Authentication} from './userAuthentication';
+
+
+export class AuthenticationController{
+    
+    constructor(
+        private userAuthentication: Authentication
+    ){}
+    
+    async handle(request: Request, response:Response): Promise<Response>{
+            const {email,password} = request.body;
+
+            try{
+
+                await this.userAuthentication.execute({email:email,password:password});
+                return response.status(200).send();
+
+            } catch(err){
+                return response.status(400).json({err});
+            }
+    }
+}
