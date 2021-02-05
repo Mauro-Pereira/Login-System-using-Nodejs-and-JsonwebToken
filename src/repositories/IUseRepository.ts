db943ff1938335b9b31806f9e625fc3156f467f9
Mauro-Pereira <mauropereira1096@gmail.com>
2021-02-04 21:02:32 -0300

    add findByPassword method

--- src/repositories/IUseRepository.ts
+++ src/repositories/IUseRepository.ts
@@ -2,5 +2,6 @@ import { User } from "../Domain/Entities/User";
 
 export interface IUseRepository{
     findByEmail(email:string): Promise<User>;
+    findByPassword(password:string): Promise<User>;
     save(user: User): Promise<void>;
 }
