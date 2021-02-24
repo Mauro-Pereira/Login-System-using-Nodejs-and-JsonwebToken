2b59a54d72718eb536ee733a0f6cd40e84e5a62c
Mauro-Pereira <mauropereira1096@gmail.com>
2021-02-24 17:43:33 -0300

    add FindByEmai and FindByPassword methods

--- src/repositories/IUseRepository.ts
+++ src/repositories/IUseRepository.ts
@@ -1,7 +1,8 @@
-import { User } from "../Domain/Entities/User";
+import { UserModelSequelize } from "../infra/sequelize/Model/UserModelSequelize";
+import {User} from "../Domain/Entities/User";
 
 export interface IUseRepository{
-    findByEmail(email:string): Promise<User>;
-    findByPassword(password:string): Promise<User>;
+    findByEmail(email:string): Promise<UserModelSequelize>;
+    findByPassword(password:string): Promise<UserModelSequelize>;
     save(user: User): Promise<void>;
 }
