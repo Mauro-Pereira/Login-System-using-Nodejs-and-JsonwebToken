23498e7cd709c4747924ebdf2e16481ff71f810c
Mauro-Pereira <mauropereira1096@gmail.com>
2021-02-26 21:56:25 -0300

    add index file

---
+++ src/useCases/createUser/index.ts
@@ -0,0 +1,8 @@
+import { DataBaseImplementation } from "../../repositories/implementation/DataBaseImplementation";
+import { CreateUserController } from "./CreateUserController";
+import { CreateUserUseCase } from "./CreateUserUseCase";
+
+const databaseimplementation = new DataBaseImplementation();
+const createUser = new CreateUserUseCase(databaseimplementation);
+
+const createUserController = new CreateUserController(createUser);
