9fbb2d1879c1508dd1b689326643174425d911ec
Mauro-Pereira <mauropereira1096@gmail.com>
2021-03-08 13:57:10 -0300

    creatind CreateUserController instance

--- src/useCases/createUser/index.ts
+++ src/useCases/createUser/index.ts
@@ -6,3 +6,5 @@ const databaseimplementation = new DataBaseImplementation();
 const createUser = new CreateUserUseCase(databaseimplementation);
 
 const createUserController = new CreateUserController(createUser);
+
+export {createUserController, createUser};
