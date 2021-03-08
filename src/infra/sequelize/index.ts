b0d4d720f04159255e5f53f22e6fcefa667e6fc5
Mauro-Pereira <mauropereira1096@gmail.com>
2021-03-08 15:41:48 -0300

    add sequelize index

--- src/infra/sequelize/index.ts
+++ src/infra/sequelize/index.ts
@@ -1,4 +1,12 @@
+import { CreateUserTable } from "./createUserTable";
 import { DataBaseConnectionImplementation } from "./DataBaseConnectionImplementation";
+import { UserModelSequelize } from "./Model/UserModelSequelize";
+
+
+const user = new UserModelSequelize();
 
 const dataBaseConnection = new DataBaseConnectionImplementation();
 dataBaseConnection.DatabaseConnection();
+
+const userTable = new CreateUserTable();
+userTable.createTable(user);
