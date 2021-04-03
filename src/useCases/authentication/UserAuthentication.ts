cddfdf274181df31fcf13e07ef015f78df30c1f0
Mauro-Pereira <mauropereira1096@gmail.com>
2021-04-03 13:56:47 -0300

    add response and request parameters in execute fuction call

--- src/useCases/authentication/UserAuthentication.ts
+++ src/useCases/authentication/UserAuthentication.ts
@@ -1,29 +1,27 @@
 import { IAuthentication } from "../../providers/authentication/IAuthentication";
 import { IUseRepository } from "../../repositories/IUseRepository";
 import { IUserAuthenticationDTO } from "./IUserAuthenticationDTO";
+import {Response,Request} from 'express';
 
 
 export class UserAuthentication{
+     
     constructor(
         private userRepository: IUseRepository,
         private authentication: IAuthentication
     ){}
 
-    async execute(data: IUserAuthenticationDTO){
+    async execute(data: IUserAuthenticationDTO, response:Response,request:Request){
     
         const verifyUser = await this.userRepository.findByEmailAndPassword(data.email,data.password);
         
         if(!verifyUser){
             throw new Error("User not Exist");
         }
-        
 
 
-       // console.log("teste")
+       await this.authentication.userAuthentication(verifyUser,response,request);
 
-      //  console.log(data.email,data.password)
-
-        await this.authentication.userAuthentication(verifyUser);
 
     }
 
