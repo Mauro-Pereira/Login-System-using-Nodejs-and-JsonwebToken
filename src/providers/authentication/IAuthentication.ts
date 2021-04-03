af2ee0ca2b0e09202f0e6408c2abe4ec4a6d174a
Mauro-Pereira <mauropereira1096@gmail.com>
2021-04-03 13:44:33 -0300

    providers/authentication/Iauthentication.ts

--- src/providers/authentication/IAuthentication.ts
+++ src/providers/authentication/IAuthentication.ts
@@ -1,6 +1,7 @@
+import { Request, Response } from "express";
 
 export interface IAuthentication{
 
     GenerateToken(params:{}): any;
-    userAuthentication(user: any): any;
+    userAuthentication(user: any,response:Response,request:Request): any;
 }
