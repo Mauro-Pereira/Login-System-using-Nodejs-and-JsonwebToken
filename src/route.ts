007118bb4e0e467df9f8496edab1b008184f4a4d
Mauro-Pereira <mauropereira1096@gmail.com>
2021-04-03 14:01:42 -0300

    add protectedRoute like a new route

--- src/route.ts
+++ src/route.ts
@@ -1,11 +1,12 @@
 import {Router} from 'express';
+import { verifyJWT } from './infra/JsonWebToken/verifyJWT';
 import { AuthenticationController } from './useCases/authentication';
 import { createUserController } from './useCases/createUser';
 
 const router = Router();
 
 router.get("/",(req, res) =>{
-    return res.status(200).send("rodando na rota principal");
+    return res.status(200).send("running on port 9001");
 })
 
 router.post("/createUser", (req, res) =>{
@@ -14,6 +15,10 @@ router.post("/createUser", (req, res) =>{
 
 router.post("/authentication",(req, res) =>{
     return AuthenticationController.handle(req, res);
+})
+
+router.get("/protectedRoute",verifyJWT,(req,res) =>{
+    return res.status(200).json("Welcome")
 })
 
 export {router}
